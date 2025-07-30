9c4b788ea4af2679625bf57bfd1972adedf6acda
boboraul <raul_de_asiss@yahoo.com>
2025-07-30 22:27:35 +0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ import { WixClientContextProvider } from "./Context/wixContext";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "e-SHop Dev E-Commerce Next.js Application",
+  title: "e-Shop Dev E-Commerce Next.js Application",
   description:
-    "A complete e-commerce application with Next.js and Wix Headless",
+    "A complete e-commerce application with Next.js, Tailwind and Wix Headless",
 };
 
 export default function RootLayout({
@@ -20,6 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Forțăm refresh la favicon prin query param */}
+        <link rel="icon" href="/favicon.ico?v=1" />
+      </head>
       <body className={inter.className}>
         <WixClientContextProvider>
           <Navbar />
