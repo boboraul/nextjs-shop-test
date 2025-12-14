5670b81b441921c5bfca885e96abd75d656a660f
boboraul <raul_de_asiss@yahoo.com>
2025-12-14 18:33:22 +0200

    route 1

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,19 +1,19 @@
 // src/app/api/login/route.ts
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export async function GET(req: Request) {
-//   const { searchParams } = new URL(req.url);
-//   const refreshToken = searchParams.get("refreshToken");
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const refreshToken = searchParams.get("refreshToken");
 
-//   if (refreshToken) {
-//     const response = NextResponse.redirect(new URL("/", req.url));
-//     response.cookies.set("refreshToken", JSON.stringify(refreshToken), {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === "production", // true în producție
-//       path: "/",
-//     });
-//     return response;
-//   }
+  if (refreshToken) {
+    const response = NextResponse.redirect(new URL("/", req.url));
+    response.cookies.set("refreshToken", JSON.stringify(refreshToken), {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // true în producție
+      path: "/",
+    });
+    return response;
+  }
 
-//   return new NextResponse("Missing refresh token", { status: 400 });
-// }
+  return new NextResponse("Missing refresh token", { status: 400 });
+}
