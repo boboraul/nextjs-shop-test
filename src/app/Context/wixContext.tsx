8ffa3bbf05d2c4c8a86afef752cc35328e0cb60e
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2025-12-16 10:27:18 +0200

    Update wixContext.tsx login issue

--- src/app/Context/wixContext.tsx
+++ src/app/Context/wixContext.tsx
@@ -1,17 +1,14 @@
 "use client";
 
+import { createContext, ReactNode, useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { createClient, OAuthStrategy, WixClient } from "@wix/sdk";
 import { products, collections } from "@wix/stores";
 import { currentCart } from "@wix/ecom";
 import * as wixData from "@wix/data";
 import { members } from "@wix/members";
-import { ReactNode, createContext } from "react";
 
-// Token refresh
-const refreshToken = Cookies.get("refreshToken");
-
-// Definim modulele clientului
+// definim modulele clientului
 type MyWixModules = {
   products: typeof products;
   collections: typeof collections;
@@ -20,35 +17,44 @@ type MyWixModules = {
   members: typeof members;
 };
 
-// Tipul clientului complet
+// tipul clientului complet
 export type MyWixClient = WixClient<undefined, any, MyWixModules>;
 
-// Cream clientul
-const wixClient: MyWixClient = createClient({
-  modules: {
-    products,
-    collections,
-    currentCart,
-    data: wixData,
-    members,
-  },
-  auth: OAuthStrategy({
-    clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
-    tokens: refreshToken ? ({ refreshToken } as any) : undefined,
-  }),
-}) as MyWixClient;
-
-// Context
+// context
 export const WixClientContext = createContext<MyWixClient | null>(null);
 
-// Provider
+// provider
 export const WixClientContextProvider = ({
   children,
 }: {
   children: ReactNode;
 }) => {
+  const [client, setClient] = useState<MyWixClient | null>(null);
+
+  useEffect(() => {
+    const refreshToken = Cookies.get("refreshToken");
+
+    const wixClient = createClient({
+      modules: {
+        products,
+        collections,
+        currentCart,
+        data: wixData,
+        members,
+      },
+      auth: OAuthStrategy({
+        clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
+        tokens: refreshToken ? ({ refreshToken } as any) : undefined,
+      }),
+    }) as MyWixClient;
+
+    setClient(wixClient);
+  }, []);
+
+  if (!client) return null;
+
   return (
-    <WixClientContext.Provider value={wixClient}>
+    <WixClientContext.Provider value={client}>
       {children}
     </WixClientContext.Provider>
   );
