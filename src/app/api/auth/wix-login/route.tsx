91d2c4915402459f26524c54df24c047c7b3a16a
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-01-13 14:32:20 +0200

    Update route.tsx from wix-login

--- src/app/api/auth/wix-login/route.tsx
+++ src/app/api/auth/wix-login/route.tsx
@@ -1,1 +1,18 @@
 
+import { NextResponse } from "next/server";
+import { createClient, OAuthStrategy } from "@wix/sdk";
+
+export async function GET() {
+  const wixClient = createClient({
+    auth: OAuthStrategy({
+      clientId: process.env.WIX_CLIENT_ID!,
+    }),
+  });
+
+  const { authorizationUrl } =
+    wixClient.auth.generateOAuthUrl({
+      redirectUri: process.env.WIX_REDIRECT_URI!,
+    });
+
+  return NextResponse.json({ authorizationUrl });
+}
