9798e18bb8c34da889f5c2cde7e826b5e653352d
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-01-16 11:30:04 +0200

    Update page.tsx logout from login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -75,76 +75,101 @@ const LoginPage = () => {
     }
   };
 
+    const handleLogOut = async () => {
+      try {
+        setIsLoading(true);
+
+        await fetch("/api/auth/logout", {
+          method: "POST",
+        });
+
+        
+        setIsLoggedIn(false);
+
+        router.refresh();
+      } catch (e) {
+        console.error("Logout failed", e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
   return (
     <div className="h-[calc(100vh-400px)] mt-10 md:mt-0 py-4 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 3xl:px-[300px] flex items-center justify-center">
-      <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
-        <h1 className="font-bold text-primary-500 text-xl">
-          {formTitle ? formTitle : "Form Title"}
-        </h1>
-
-        {loggedIn && (
-          <div className="text-success-500 text-sm">
-            <p>You are logged in</p>
+    {loggedIn ? (
+          <div className="loggedin">
+            <p className="text-success-500 text-sm">You are logged in</p>
+             <button
+            disabled={isLoading} onClick={handleLogOut}
+            className="bg-primary-500 mt-4 text-white rounded-md p-1 disabled:bg-gray-300 disabled:cursor:not-allowed"
+            >
+              Log out
+            </button>
           </div>
-        )}
+        ) : (
+        <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+          <h1 className="font-bold text-primary-500 text-xl">
+            {formTitle ? formTitle : "Form Title"}
+          </h1>
 
-        <div className="form-group flex flex-col gap-1">
-          <label className="text-md text-gray-500" htmlFor="">
-            Email
-          </label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-            name="email"
-            placeholder="billy@gmail.com"
-            className="ring-2 ring-gray-300 rounded-md p-2"
-          />
-        </div>
+          <div className="form-group flex flex-col gap-1">
+            <label className="text-md text-gray-500" htmlFor="">
+              Email
+            </label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              name="email"
+              placeholder="billy@gmail.com"
+              className="ring-2 ring-gray-300 rounded-md p-2"
+            />
+          </div>
 
-        <div className="form-group flex flex-col gap-1">
-          <label className="text-md text-gray-500" htmlFor="">
-            Password
-          </label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-            name="password"
-            placeholder="password"
-            className="ring-2 ring-gray-300 rounded-md p-2"
-          />
-        </div>
+          <div className="form-group flex flex-col gap-1">
+            <label className="text-md text-gray-500" htmlFor="">
+              Password
+            </label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              name="password"
+              placeholder="password"
+              className="ring-2 ring-gray-300 rounded-md p-2"
+            />
+          </div>
 
-        <button
-          disabled={isLoading}
-          className="bg-primary-500 mt-4 text-white rounded-md p-1 disabled:bg-gray-300 disabled:cursor:not-allowed"
-        >
-          {btnTitle}
-        </button>
-
-        {error && <div className="text-danger-500 font-semibold">{error}</div>}
-
-        {mode === MODE.LOGIN && (
-          <div
-            className="underline text-primary-500 cursor-pointer text-xs"
-            onClick={() => setMode(MODE.REGISTER)}
+          <button
+            disabled={isLoading}
+            className="bg-primary-500 mt-4 text-white rounded-md p-1 disabled:bg-gray-300 disabled:cursor:not-allowed"
           >
-            Don`t have an account?
-          </div>
-        )}
+            {btnTitle}
+          </button>
 
-        {mode === MODE.REGISTER && (
-          <div
-            className="underline text-primary-500 cursor-pointer text-xs"
-            onClick={() => setMode(MODE.LOGIN)}
-          >
-            Have an account?
-          </div>
-        )}
+          {error && <div className="text-danger-500 font-semibold">{error}</div>}
 
-        {!error && message && (
-          <div className="text-success-500 text-sm">{message}</div>
+          {mode === MODE.LOGIN && (
+            <div
+              className="underline text-primary-500 cursor-pointer text-xs"
+              onClick={() => setMode(MODE.REGISTER)}
+            >
+              Don`t have an account?
+            </div>
+          )}
+
+          {mode === MODE.REGISTER && (
+            <div
+              className="underline text-primary-500 cursor-pointer text-xs"
+              onClick={() => setMode(MODE.LOGIN)}
+            >
+              Have an account?
+            </div>
+          )}
+
+          {!error && message && (
+            <div className="text-success-500 text-sm">{message}</div>
+          )}
+        </form>
         )}
-      </form>
     </div>
   );
 };
