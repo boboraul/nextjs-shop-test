256776e6a51ddc0b89e5d5af3765146db16ea49b
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-01-20 12:33:59 +0200

    Update ProductList.tsx 1/2 1/4

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -70,7 +70,7 @@ const ProductList = async ({
         <React.Fragment key={product._id}>
           <Link
             href={"/" + product.slug}
-            className="w-full flex-col gap-44 sm:w-[45%] lg:w-1/6"
+            className="w-full flex-col gap-44 sm:w-1/2 lg:w-1/4"
           >
             <div className="w-full h-80 relative">
               <Image
