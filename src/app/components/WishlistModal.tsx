c37b36246599cf46ce846609b1bd3d5b0af6e5b5
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-01-22 13:39:40 +0200

    Update WishlistModal.tsx wishmodal min-w

--- src/app/components/WishlistModal.tsx
+++ src/app/components/WishlistModal.tsx
@@ -23,7 +23,7 @@ const WishlistModal = () => {
   console.log('wishmodal items ', items)
 
   return (
-    <div className="absolute bg-white min-w-[350px] border-t cursor-default rounded-md right-0 shadow-md p-3 top-7 text-sm z-20">
+    <div className="absolute bg-white min-w-[250px] border-t cursor-default rounded-md right-0 shadow-md p-3 top-7 text-sm z-20">
       <h4 className="text-sm pb-2 border-b">Wishlist</h4>
       {isLoading ? (
         "Loading..."
