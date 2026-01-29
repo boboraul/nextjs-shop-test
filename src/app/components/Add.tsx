7b32bbdd017cd4b95341d384fc2c42a6d4339bb9
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-01-29 14:55:09 +0200

    Update Add.tsx use effect

--- src/app/components/Add.tsx
+++ src/app/components/Add.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useCartStore } from "../hooks/useCartStore";
 import AddToWishlistButton from "./AddToWishlistButton";
 
