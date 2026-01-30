7c8e00ed67b6299e5c454f81c5a66ccb306d2975
boboraul <raul_de_asiss@yahoo.com>
2026-01-30 21:24:44 +0200

    max age cookie

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -47,6 +47,7 @@ export async function POST(req: Request) {
     httpOnly: true,
     sameSite: "lax",
     secure: process.env.NODE_ENV === "production",
+    maxAge: 60 * 60 * 24 * 30, // 30 days
     path: "/",
   });
 
