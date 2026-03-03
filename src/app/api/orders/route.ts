3e02bfa8dbec0ee8ff556c878d1a91113f91acfe
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-03-03 13:56:26 +0200

    Update route.ts
    currency in zod

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -17,7 +17,8 @@ const OrderSchema = z.object({
         variantId: z.string().optional(),
         qty: z.number().int().positive().max(50),
         productName: z.string().min(1),
-        price: z.string().min(1)
+        price: z.number().int().positive(),
+        currency: z.string().min(1)  
       })
     )
     .min(1),
