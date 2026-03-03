a1be229a4f9877ad83d4b2eb3620df9636cea889
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-03-03 14:49:18 +0200

    Update route.ts
    zod shipping values

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -23,11 +23,11 @@ const OrderSchema = z.object({
     firstName: z.string().min(1),
     lastName: z.string().min(1),
     email: z.string().email(),
-    phone: z.string().min(1),
+    phone: z.number().int().min(3),
     country: z.string().min(1),
     city: z.string().min(1),
     address: z.string().min(1),
-    postalCode: z.string().min(1)
+    postalCode: z.number().min(3)
   }),
   totalPrice: z.number().positive(),
   paymentMethod: z.enum(["card", "cash_on_delivery"]),
