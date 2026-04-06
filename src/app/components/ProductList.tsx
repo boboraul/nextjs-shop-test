8a3e085f629efa9a925cc3380d0f1e376559a5ef
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-04-06 15:46:18 +0300

    || 0

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -86,7 +86,7 @@ const ProductList = async ({
       ))}
       {searchParams && (
         <Pagination
-          currentPage={res.currentPage || ''}
+          currentPage={res.currentPage || 0}
           hasPrev={res.hasPrev()}
           hasNext={res.hasNext()}
         />
