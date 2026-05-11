b902d9751dab7b1d0a36561b07862f81b7c24d09
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-05-11 14:51:21 +0300

    hp margins 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,18 @@ import CategoryList from "./components/CategoryList";
 const HomePage = async () => {
 
   return (
-    <div className="">
+    <div className="Homepage">
     
       <Slider />
 
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 3xl:px-[300px]">
+      <div className="mt-10 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 3xl:px-[300px]">
 
          <div className="homepage-title mt-5 text-center">
             <h1 className="text-2xl font-bold">Discover Curated Products for a Smarter Shopping Experience</h1>
          </div>
         
         <Suspense fallback={"Loading..."}>
-           <div className="mt-20">
+           <div className="mt-10">
             <CarouselProducts
               categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
               limit={4}
@@ -34,7 +34,7 @@ const HomePage = async () => {
         </Suspense>
       </div>
 
-      <div className="mt-24">
+      <div className="mt-10">
 
         <div className="text-3xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 3xl:px-[300px] mb-10">
           <<div className="text-2xl">
