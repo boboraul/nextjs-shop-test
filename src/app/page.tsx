97506bcdff0d6e8d4bc4e5cd1ac9546abc57c666
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-05-11 14:58:14 +0300

    hp h1 title margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const HomePage = async () => {
 
       <div className="mt-10 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 3xl:px-[300px]">
 
-         <div className="homepage-title mt-5 text-center">
+         <div className="homepage-title my-20 text-center">
             <h1 className="text-2xl font-bold">Discover Curated Products for a Smarter Shopping Experience</h1>
          </div>
         
