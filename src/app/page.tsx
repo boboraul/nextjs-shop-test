2abfdeb549225e93e59e045cdf10a5d81362b95f
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-05-11 14:41:39 +0300

    hp margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,12 @@ const HomePage = async () => {
 
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 3xl:px-[300px]">
 
-         <div className="homepage-title mt-3 text-center">
+         <div className="homepage-title mt-10 text-center">
             <h1>Discover Curated Products for a Smarter Shopping Experience</h1>
          </div>
         
         <Suspense fallback={"Loading..."}>
-           <div className="my-5">
+           <div className="mt-10">
             <CarouselProducts
               categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
               limit={4}
@@ -27,7 +27,7 @@ const HomePage = async () => {
             />
           </div>
 
-          <div className="my-5">
+          <div className="mt-10">
             <CarouselProducts carouselTitle={'New in'} limit={10} />
           </div>
           
