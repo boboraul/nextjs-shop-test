2f8629dcdfc24c2bc0c49ede7b1d8a06ab1efc06
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-05-13 12:12:28 +0300

    oldest products

--- src/app/components/CarouselProducts.tsx
+++ src/app/components/CarouselProducts.tsx
@@ -24,7 +24,7 @@ export default async function CarouselProducts({ carouselTitle, limit, categoryI
   if (sortKey) {
     if (sortKey === "lastUpdated") {
       items.sort((a: any, b: any) => 
-        new Date(b.lastUpdated).getTime() - new Date(a.lastUpdated).getTime(),
+        new Date(a.lastUpdated).getTime() - new Date(b.lastUpdated).getTime(),
       );
     }
   }
@@ -32,7 +32,6 @@ export default async function CarouselProducts({ carouselTitle, limit, categoryI
   const filteredProducts = items.filter((product) => {
     return product.visible !== false;
   });
-
 
   return (
     <Carousel
