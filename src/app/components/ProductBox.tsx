d80e7d0dac8a84bc00b148812792babb33503687
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-05-14 15:41:22 +0300

    ratings font size

--- src/app/components/ProductBox.tsx
+++ src/app/components/ProductBox.tsx
@@ -94,7 +94,7 @@ export default function ProductBox({
           {[1, 2, 3, 4, 5].map((index) => (
             <StarIcon
               key={index}
-              className={`h-5 w-5 ${
+              className={`h-3 w-3 ${
                 index <= ratingScore 
                   ? 'text-amber-400'  // Galben/Auriu pentru rating activ
                   : 'text-gray-300'   // Gri pentru rating inactiv
