1277f655c3ea7eb67a2ed226d0ad4539380ccbf9
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-05-27 11:45:52 +0300

    spin 3

--- src/app/components/Spinner.tsx
+++ src/app/components/Spinner.tsx
@@ -3,8 +3,8 @@ import { ArrowPathIcon } from "@heroicons/react/24/solid";
 
 export function Spinner({ className = "h-6 w-6 text-primary-500" }: { className?: string }) {
   return (
-    <div className="text-center w-full">
-      <ArrowPathIcon className={`animate-spin text-center ${className}`} aria-hidden="true" />
+    <div className="text-center">
+      <ArrowPathIcon className={`animate-spin inline-block ${className}`} aria-hidden="true" />
     </div>
   );
 }
