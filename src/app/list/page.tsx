3382cd2902ac007b522c78ab3728750ad73d6e25
Vasilescu Bogdan <43149383+boboraul@users.noreply.github.com>
2026-05-27 11:37:07 +0300

    spin

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -50,6 +50,7 @@ const ListPage = async ({ searchParams }: { searchParams: Promise<any> }) => {
       <Filter />
       {/* Products */}
       <h1 className="mt-4 text-xl font-semibold">{cat?.collection?.name}</h1>
+      <Spinner />
       <Suspense fallback={<Spinner />}>
         <ProductList
           categoryId={cat.collection?._id || ALL_PRODUCTS_ID}
